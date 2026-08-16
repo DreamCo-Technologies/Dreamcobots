16c26c22af396bf587a0515017198c9b86604eb9
Irean jirdan <ireanjordan28@gmail.com>
2026-08-16 07:39:22 -0500

    feat: add DreamCo source learning package foundation

---
+++ framework/dreamco_source_learning/__init__.py
@@ -0,0 +1,17 @@
+"""DreamCo Source Learning -> Original Capability Package pipeline."""
+
+from .pipeline import (
+    LicenseStatus,
+    SourceRecord,
+    LearningArtifact,
+    CapabilityPackage,
+    SourceLearningPipeline,
+)
+
+__all__ = [
+    "LicenseStatus",
+    "SourceRecord",
+    "LearningArtifact",
+    "CapabilityPackage",
+    "SourceLearningPipeline",
+]
